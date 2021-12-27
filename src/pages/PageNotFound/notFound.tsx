f885edd5931da050bd9e6db7c0a8b454a56561fe
Sarthak Shukla <sarthak2019fractal@gmail.com>
2021-12-27 21:26:34 +0530

    Page Not Found Changes

--- src/pages/PageNotFound/notFound.tsx
+++ src/pages/PageNotFound/notFound.tsx
@@ -3,11 +3,11 @@ import Header from "../../components/molecules/header";
 import styled from "styled-components";
 import { Ink } from "../../components/vectors";
 import { useHistory } from "react-router";
+import NotFound from "./../../components/vectors/NotFound";
 const AddClientContainer = styled.div`
   display: flex;
-  flex-direction: column;
   align-items: center;
-  justify-content: center;
+  flex-direction: column;
   background-color: var(--black);
   width: 100%;
   min-height: 100vh;
@@ -17,29 +17,33 @@ const AddText = styled.div`
   font-size: 35.102px;
   text-align: center;
   color: #ffffff;
+  margin-top: 40px;
 `;
 const AddText1 = styled.div`
   font-weight: 500;
-  font-size: 25.102px;
+  font-size: 35.102px;
   text-align: center;
   color: #ffffff;
-  cursor: pointer;
 `;
-const NotFound = () => {
+const NoSource = () => {
   const history = useHistory();
   return (
     <AddClientContainer>
-      <Ink width="272px" height="342px" />
-      <AddText>Page Not Found !</AddText>
-      <AddText1
-        onClick={(e) => {
+      <Header
+        heading="Ooops !"
+        subheading="@WW24"
+        buttonText="Go to Home Page"
+        buttonHandler={() => {
           history.push("/");
         }}
-      >
-        Click Here To Go to Home Page
-      </AddText1>
+      />
+      <div style={{ marginTop: "20px" }}>
+        <NotFound />
+      </div>
+      <AddText>This page is not available</AddText>
+      <AddText1>It seems something went wrong....</AddText1>
     </AddClientContainer>
   );
 };
 
-export default NotFound;
+export default NoSource;
